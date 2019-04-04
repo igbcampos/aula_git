3b770b1bdc7e31f5b00c8b5fde61143218de967c
Guilherme Avelino <gavelino@gmail.com>
2019-04-04 10:35:24 -0300

    add opcao de saida #1

--- Main.java
+++ Main.java
@@ -3,8 +3,12 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) {
-		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
-		System.out.println(mainMenu.getSelection() + "foi selecionada");
+		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes", "Sair"));
+		int op=0;
+		do {
+			op = mainMenu.getSelection();
+			System.out.println( op + "foi selecionada");
+		} while(op!=4);
 		System.out.println("Fim");
 	}
 
